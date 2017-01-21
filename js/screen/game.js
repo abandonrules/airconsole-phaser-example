3582e91f374068283d7f27139ec105ca9de5d287
mclubb <mclubb@charter.net>
2017-01-21 03:40:35 -0600

    Stuff

--- js/screen/game.js
+++ js/screen/game.js
@@ -236,102 +236,35 @@ function create () {
     land.height = game.height;
     land.fixedToCamera = true;
 
-    // Scale
-  /*  var scale_manager = new Phaser.ScaleManager(game, 800, 600);
-    scale_manager.scaleMode = Phaser.ScaleManager.RESIZE;
-    scale_manager.pageAlignVertically = true;
-    scale_manager.pageAlignHorizontally = true;
-    scale_manager.refresh();
-*/
     createPlanets();
 
-    //  The base of our tank
-    /*tank = game.add.sprite(0, 0, 'tank', 'tank1');
-    tank.anchor.setTo(0.5, 0.5);
-    tank.animations.add('move', ['tank1', 'tank2', 'tank3', 'tank4', 'tank5', 'tank6'], 20, true);
-
-    //  This will force it to decelerate and limit its speed
-    game.physics.enable(tank, Phaser.Physics.ARCADE);
-    tank.body.drag.set(0.2);
-    tank.body.maxVelocity.setTo(400, 400);
-    tank.body.collideWorldBounds = true;
-
-    //  Finally the turret that we place on-top of the tank body
-    turret = game.add.sprite(0, 0, 'tank', 'turret');
-    turret.anchor.setTo(0.3, 0.5);
-
-    //  The enemies bullet group
-    enemyBullets = game.add.group();
-    enemyBullets.enableBody = true;
-    enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
-    enemyBullets.createMultiple(100, 'bullet');
-
-    enemyBullets.setAll('anchor.x', 0.5);
-    enemyBullets.setAll('anchor.y', 0.5);
-    enemyBullets.setAll('outOfBoundsKill', true);
-    enemyBullets.setAll('checkWorldBounds', true);
-
-    //  Create some baddies to waste :)
-    enemies = [];
-
-    enemiesTotal = 20;
-    enemiesAlive = 20;
-
-    for (var i = 0; i < enemiesTotal; i++)
-    {
-        enemies.push(new EnemyTank(i, game, tank, enemyBullets));
-    }
-
-    //  A shadow below our tank
-    shadow = game.add.sprite(0, 0, 'tank', 'shadow');
-    shadow.anchor.setTo(0.5, 0.5);
-
-    //  Our bullet group
-    bullets = game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    bullets.createMultiple(30, 'bullet', 0, false);
-    bullets.setAll('anchor.x', 0.5);
-    bullets.setAll('anchor.y', 0.5);
-    bullets.setAll('outOfBoundsKill', true);
-    bullets.setAll('checkWorldBounds', true);
-
-    //  Explosion pool
-    explosions = game.add.group();
-
-    for (var i = 0; i < 10; i++)
-    {
-        var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
-        explosionAnimation.anchor.setTo(0.5, 0.5);
-        explosionAnimation.animations.add('kaboom');
-    }
-    */
-/*
-    tank.bringToTop();
-    turret.bringToTop();
-*/
     logo = game.add.sprite(300, 200, 'logo');
     logo.fixedToCamera = true;
-
-    // This is now handled by AirConsole
-    // game.input.onDown.add(removeLogo, this);
-
-  //  game.camera.follow(tank);
     game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
     game.camera.focusOnXY(0, 0);
 
-    // This is now handled by AirConsole onMessage
-    // cursors = game.input.keyboard.createCursorKeys();
 }
 
-function removeLogo() {
-    logo.kill();
+function update () {
+  if( players.length <= 1 )
+  {
+    if( !logo )
+    {
+      logo = game.add.sprite(300, 200, 'logo');
+    }
+  } else {
+    if( logo )
+    {
+      removeLogo();
+    }
+  }
 }
 
-function DestroySprite(sprite)
-{
-  sprite.destroy();
+function render () {
+    //game.debug.text('Enemies: ' + enemiesAlive + ' / ' + enemiesTotal, 32, 32);
+    game.debug.text("Players connected: " + players.length, 32, 32);
 }
+
 
 function createPlanets()
 {
@@ -347,104 +280,11 @@ function createPlanets()
   }
 }
 
-function update () {
-
-    //game.physics.arcade.overlap(enemyBullets, tank, bulletHitPlayer, null, this);
-/*
-    enemiesAlive = 0;
-
-    for (var i = 0; i < enemies.length; i++)
-    {
-        if (enemies[i].alive)
-        {
-            enemiesAlive++;
-            game.physics.arcade.collide(tank, enemies[i].tank);
-            game.physics.arcade.overlap(bullets, enemies[i].tank, bulletHitEnemy, null, this);
-            enemies[i].update();
-        }
-    }
-
-    // Instead of checking for keyboard input we check the state of the devices-object
-    // and which buttons are pressed
-
-    // If the driver-device e.g. pressed the left button, then the tank turns left
-    // Previously: cursors.left.isDown
-    if (driver_device.left > 0) {
-        tank.angle -= 4;
-    }
-    // Previously: cursors.right.isDown
-    else if (driver_device.right > 0) {
-        tank.angle += 4;
-    }
-
-    // Previously: cursors.up.isDown
-    if (driver_device.up) {
-        //  The speed we'll travel at
-        currentSpeed = 300;
-    } else {
-        if (currentSpeed > 0) {
-            currentSpeed -= 4;
-        }
-    }
-
-    if (currentSpeed > 0) {
-        game.physics.arcade.velocityFromRotation(tank.rotation, currentSpeed, tank.body.velocity);
-    }
-
-    land.tilePosition.x = -game.camera.x;
-    land.tilePosition.y = -game.camera.y;
-
-    //  Position all the parts and align rotations
-    shadow.x = tank.x;
-    shadow.y = tank.y;
-    shadow.rotation = tank.rotation;
-
-    turret.x = tank.x;
-    turret.y = tank.y;
-
-    // Check which buttons the shooter currently pressed
-    if (shooter_device.left > 0) {
-        turret.angle -= 3;
-    } else if (shooter_device.right > 0) {
-        turret.angle += 3;
-    }
-    */
-}
-/*
-// A bullet hits the players tank
-function bulletHitPlayer (tank, bullet) {
-    bullet.kill();
+function removeLogo() {
+    logo.destroy();
 }
 
-// A bullet hits the enemy
-function bulletHitEnemy (tank, bullet) {
-    bullet.kill();
-    var destroyed = enemies[tank.name].damage();
-    if (destroyed) {
-        var explosionAnimation = explosions.getFirstExists(false);
-        explosionAnimation.reset(tank.x, tank.y);
-        explosionAnimation.play('kaboom', 30, false, true);
-    }
-}
-
-// Called when the Shooter presses both buttons (left and right)
-function fire () {
-    if (game.time.now > nextFire && bullets.countDead() > 0) {
-        nextFire = game.time.now + fireRate;
-        var bullet = bullets.getFirstExists(false);
-        bullet.reset(turret.x, turret.y);
-        // We don't play this game any more by mouse, so we have to calculate the direction
-        // of the bullet in another way
-        var rad = turret.angle * (Math.PI / 180);
-        var distance = 400;
-        var x = turret.x + distance * Math.cos(rad);
-        var y = turret.y + distance * Math.sin(rad);
-        // Previous: game.physics.arcade.moveToPointer(bullet, 1000, game.input.activePointer, 500);
-        bullet.rotation = game.physics.arcade.moveToXY(bullet, x, y, 1000, 500);
-    }
-}
-*/
-function render () {
-    //game.debug.text('Enemies: ' + enemiesAlive + ' / ' + enemiesTotal, 32, 32);
-    game.debug.text("Players connected: " + players.length, 32, 32);
+function DestroySprite(sprite)
+{
+  sprite.destroy();
 }
