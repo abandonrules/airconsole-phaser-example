9090dba422fe891a779a21c97e09c32004c94334
mclubb <mclubb@charter.net>
2017-01-21 14:53:56 -0600

    Changed anchor point

--- spacecase/js/screen/game.js
+++ spacecase/js/screen/game.js
@@ -138,7 +138,8 @@ function create () {
       player.body.setCircle(50);
       player.setHealth(250);
       player.scale.set(0.5, 0.5);
-      player.anchor.setTo(0.5, 0.5);
+      //player.anchor.setTo(0.5, 0.5);
+      player.anchor.setTo(centerX, centerY);
       player.body.setCollisionGroup(playersCollisionGroup);
       player.body.collides([planetsCollisionGroup, playersCollisionGroup], playerHit, this);
       players[device_id] = player;
