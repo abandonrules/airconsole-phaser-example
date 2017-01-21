9b6e59dd8d8768883afb304d21671a2dc0cdc15e
mclubb <mclubb@charter.net>
2017-01-20 23:01:47 -0600

    Testing player connect and removal of alot of code

--- js/screen/game.js
+++ js/screen/game.js
@@ -22,14 +22,23 @@ function preload () {
     game.load.image('bullet', 'assets/game/bullet.png');
     game.load.image('earth', 'assets/game/scorched_earth.png');
     game.load.spritesheet('kaboom', 'assets/game/explosion.png', 64, 64, 23);
+    game.load.image('cat1', 'assets/game/cat2.png');
+    game.load.image('cat2', 'assets/game/cat3.png');
+    game.load.image('cat3', 'assets/game/cat4.png');
+    game.load.image('cat4', 'assets/game/cat5.png');
+    game.load.image('cat5', 'assets/game/cat6.png');
+    game.load.image('cat6', 'assets/game/cat7.png');
+    game.load.image('cat7', 'assets/game/cat8.png');
+    game.load.image('cat8', 'assets/game/cat9.png');
 }
 
 var land;
 
 var shadow;
-var tank;
+//var tank;
 var turret;
 
+var players;
 var enemies;
 var enemyBullets;
 var enemiesTotal = 0;
@@ -51,7 +60,7 @@ var airconsole = null;
 
 // A map that holds the device_ids of the driver and the shooter
 var device_control_map = [];
-
+/*
 // A button-state map which indicates which buttons the driver has currently pressed
 var driver_device = {
     left: 0,
@@ -66,7 +75,7 @@ var shooter_device = {
     fire: false
 };
 // ------------------------
-
+*/
 function create () {
 
     // =======================================================
@@ -74,17 +83,20 @@ function create () {
     // =======================================================
 
     // Send a message to each device to tell them the role (tank or shooter)
-    var setRoles = function() {
+  /*  var setRoles = function() {
         for (var i = 0; i < device_control_map.length; i++) {
-            // We only allow two players in this game
-            if (i >= 2) break;
+            // We only allow 8 players in this game
+            if (i >= 8) break;
             var device_id = device_control_map[i];
+            /*
             airconsole.message(device_id, {
                 action: "SET_ROLE",
                 role: i === 0 ? 'DRIVER' : 'SHOOTER'
             });
+
         }
     };
+    */
 
     airconsole = new AirConsole();
     airconsole.onReady = function() {};
@@ -92,12 +104,12 @@ function create () {
     // As soon as a device connects we add it to our device-map
     airconsole.onConnect = function(device_id) {
         // Only first two devices can play
-        if (device_control_map.length < 2) {
+        if (device_control_map.length < 8) {
             device_control_map.push(device_id);
+            players.push(new Player(device_id, game, players, bullets));
             // Send a message back to the device, telling it which role it has (tank or shooter)
-            setRoles();
+            //setRoles();
         }
-        removeLogo();
     };
 
     // Called when a device disconnects (can take up to 5 seconds after device left)
@@ -107,12 +119,24 @@ function create () {
         if (index !== -1) {
             device_control_map.splice(index, 1);
             // Update roles
-            setRoles();
+            //setRoles();
+            // remove player
+            var deleteIndex;
+            for( var i = 0; i < players.length; i++ )
+            {
+              if( players[i].index == device_id )
+              {
+                deleteIndex = i;
+              }
+            }
+
+            players.splice(deleteIndex, 1);
         }
     };
 
     // onMessage is called everytime a device sends a message with the .message() method
     airconsole.onMessage = function(device_id, data) {
+      /*
         // First in the array is always the driver
         var driver  = device_control_map[0];
         // Second in the array is always the shooter
@@ -151,6 +175,8 @@ function create () {
                 fire();
             }
         }
+
+        */
     };
 
     // =======================================================
@@ -172,7 +198,7 @@ function create () {
     scale_manager.refresh();
 
     //  The base of our tank
-    tank = game.add.sprite(0, 0, 'tank', 'tank1');
+    /*tank = game.add.sprite(0, 0, 'tank', 'tank1');
     tank.anchor.setTo(0.5, 0.5);
     tank.animations.add('move', ['tank1', 'tank2', 'tank3', 'tank4', 'tank5', 'tank6'], 20, true);
 
@@ -231,17 +257,18 @@ function create () {
         explosionAnimation.anchor.setTo(0.5, 0.5);
         explosionAnimation.animations.add('kaboom');
     }
-
+    */
+/*
     tank.bringToTop();
     turret.bringToTop();
-
+*/
     logo = game.add.sprite(300, 200, 'logo');
     logo.fixedToCamera = true;
 
     // This is now handled by AirConsole
     // game.input.onDown.add(removeLogo, this);
 
-    game.camera.follow(tank);
+  //  game.camera.follow(tank);
     game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
     game.camera.focusOnXY(0, 0);
 
@@ -255,8 +282,8 @@ function removeLogo() {
 
 function update () {
 
-    game.physics.arcade.overlap(enemyBullets, tank, bulletHitPlayer, null, this);
-
+    //game.physics.arcade.overlap(enemyBullets, tank, bulletHitPlayer, null, this);
+/*
     enemiesAlive = 0;
 
     for (var i = 0; i < enemies.length; i++)
@@ -314,8 +341,9 @@ function update () {
     } else if (shooter_device.right > 0) {
         turret.angle += 3;
     }
+    */
 }
-
+/*
 // A bullet hits the players tank
 function bulletHitPlayer (tank, bullet) {
     bullet.kill();
@@ -348,7 +376,7 @@ function fire () {
         bullet.rotation = game.physics.arcade.moveToXY(bullet, x, y, 1000, 500);
     }
 }
-
+*/
 function render () {
-    game.debug.text('Enemies: ' + enemiesAlive + ' / ' + enemiesTotal, 32, 32);
+    //game.debug.text('Enemies: ' + enemiesAlive + ' / ' + enemiesTotal, 32, 32);
 }
