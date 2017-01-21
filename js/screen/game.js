4abd86f4bac1514daef115c45c0784c7d6e37729
mclubb <mclubb@charter.net>
2017-01-21 16:20:59 -0600

    YOLO

--- js/screen/game.js
+++ js/screen/game.js
@@ -58,7 +58,7 @@ function create () {
     bullets.createMultiple(20, "bullet");
     for( var i = 0; i < bullets.children.length; i++ ) {
       bullets.children[i].body.setCollisionGroup(bulletsCollisionGroup);
-      bullets.children[i].body.collides([planetsCollisionGroup, playersCollisionGroup, bulletsCollisionGroup], bulletHit, this);
+      bullets.children[i].body.collides([planetsCollisionGroup, bulletsCollisionGroup], bulletHit, this);
     }
     bullets.callAll('events.onOutOfBounds.add', 'events.onOutOfBounds', resetBullet);
     bullets.callAll('anchor.setTo', 'anchor', 0.5, 1.0);
